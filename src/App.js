73f6a35165b90ea9e40a5d0447bee66e844ad8ec
alvaaroofs <afernandezs@uxcale.com>
2024-07-22 12:40:14 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Alvaro" //si le meto <h1>Alvaro</h1> como valor, con poner {name} se me guarda
+  const age = <h2> 21</h2>
+  const email = <h3>afernandezs@uxcale.com</h3>
+  const description = <div>Frase de prueba para ver como queda el texto escrito</div>
+  const user = (
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactasasas
-        </a>
-      </header>
+  <div className="App"> Empezando React; 
+    <h1>{name}</h1>
+    <h2>{name}</h2>
+    <h3>{name}</h3>
+    <h4>{name}</h4>
+    <h5>{name}</h5>
+    <h6>{name}</h6>
+    <div>
+      {age}
+      {email}
+      {description}
+    </div>
+      {user}
+      <User name="Paco" age={400} email="example@example.com"/>
+      <Salary quantity="500000" nameJob="Programmer" enterprise="Netflix"/>
+      
+  </div>
+  );
+}
+//Para llamar al componente User, se haria de forma abreviada <User />
+
+//Para crear componentes, se hace con const (nombre **primera en mayuscula) = () => {};
+
+//Los props los props son mecanismos que permiten transmitir datos
+//desde un componente principal a un componente secundario (Al llamarlo en el App, le paso los atributos y ya)
+//Ej:
+
+const User = (props) =>{
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.email}</h3>
     </div>
   );
 }
 
+const Salary = (props) => {
+  return (
+    <div>
+      <h1>{props.quantity}</h1>
+      <h1>{props.nameJob}</h1>
+      <h1>{props.enterprise}</h1>
+    </div>
+  );
+}
 export default App;
