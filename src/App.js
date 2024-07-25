119886d2fdc1ba7a54272d6d8b910247a410c660
alvaaroofs <afernandezs@uxcale.com>
2024-07-25 13:50:03 +0200

    Apartado 7, example 2

--- src/App.js
+++ src/App.js
@@ -4,29 +4,38 @@ import { useEffect, useState } from "react";
 
 //**Con la funcion fetch(), importamos los datos de una API
 //==========================================
-//7. Obtener datos de una API usando React, para sacarlo por pantalla:
-//Utilizaremos una libreria para fetch data de forma mas sencilla, instalamos el paquete
-//mediante npm install axios y tras instalarlo, metemos un import Axios from "axios";
-//en la zona de los imports de App.js
+//7.2 Ejemplo; a partir de la pagina https://api.agify.io/?name=alvaro
 function App() {
-  const [catFact, setCatFact] = useState("");
+//Un placeholder es como un texto de ejemplo para que el cuadro de input no este vacio
+//le pasamos el enlace con el fetchData, con ...name= pero sin ningun nombre, ya que
+//lo vamos a añadir nosotros mismos con el input
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
 
-
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      console.log(res.data.fact);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
     });
   };
+//Como vamos a modificar el name, con el input, para añadirlo al enlace de la linea 16, hay que poner estas comillas ``, ya que si ponemos las dobles, no nos pillara lo que le añadamos
 
-  useEffect(() => {
-      fetchCatFact();
-  }, []);
-
+//De este modo, cada vez que añadamos un nuevo <input/>, el const [name ...] cambiara
 return (
   <div className="App">
-    <button> Generate Cat Fact </button>
-    <p> {catFact} </p>
-  </div>
-)};
+    <input 
+      placeholder="Example: Alvaro..."
+      onChange={(event) => {
+        setName(event.target.value);
+      }}
+    />
+    <button onClick={fetchData}> Age predict </button>
 
+    <h1> Name: {predictedAge?.name} </h1>
+    <h1> Predicted Age: {predictedAge?.age} </h1>
+    <h1> Count: {predictedAge?.count} </h1>
+
+  </div>
+  //Al meter la ? despues de predictedAge (u objeto), le estamos diciendo a la API que solamente nos lo de si el useState no es null, ya que al principio lo hemos establecido asi
+);
+}
 export default App;
