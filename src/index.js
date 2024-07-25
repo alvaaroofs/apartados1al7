082640a60bbdd7c0637270047147a6a131a1a95a
alvaaroofs <afernandezs@uxcale.com>
2024-07-25 10:25:37 +0200

    apartado 6 finalizado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+//El React.StrictMode, es recomendable dejarlo, ya que nos ayuda a escribir un mejor codigo
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
